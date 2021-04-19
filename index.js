3958f6bdb4e6d53bcaf15e6c65c388892ae33d41
limyeonsoo <yslim6168@naver.com>
2021-04-19 17:46:33 +0900

    MongoDB Connect

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
-const express = require('express')
+const express = require('express');
+const path = require('path');
+const dotenv = require('dotenv');
 const app = express()
 const port = 4000
+
+dotenv.config({
+    path: path.resolve(
+      process.cwd(),
+      ".env"
+    )
+  });
+
+const mongoose = require('mongoose');
+
+
+mongoose.connect(`mongodb+srv://${process.env.DBID}:${process.env.DBPW}@madgo.nvzw2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`, {
+    // connect option
+    useNewUrlParser : true,
+    useUnifiedTopology : true,
+    useCreateIndex : true,
+    useFindAndModify: false
+}).then(() => console.log("MongoDB Connected!!!"))
+.catch(err => console.log("MongoDB Error!!!"))
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
