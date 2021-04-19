5a48889f431a39d00531b24225680d9ccd0f847b
limyeonsoo <yslim6168@naver.com>
2021-04-19 20:10:05 +0900

    Register (post)

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 const express = require('express');
 const path = require('path');
 const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
 const app = express()
 const port = 4000
 
 dotenv.config({
     path: path.resolve(
-      process.cwd(),
-      ".env"
+        process.cwd(),
+        ".env"
     )
-  });
+});
 
 const mongoose = require('mongoose');
-
+const { Users } = require('./models/Users');
 
 mongoose.connect(`mongodb+srv://${process.env.DBID}:${process.env.DBPW}@madgo.nvzw2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`, {
-    // connect option
+    // connect option  
     useNewUrlParser : true,
     useUnifiedTopology : true,
     useCreateIndex : true,
@@ -23,9 +24,23 @@ mongoose.connect(`mongodb+srv://${process.env.DBID}:${process.env.DBPW}@madgo.nv
 }).then(() => console.log("MongoDB Connected!!!"))
 .catch(err => console.log("MongoDB Error!!!"))
 
+// For application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+// For application/json
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/register', (req, res) => {
+    // client request with information for Sign Up.
+
+    const user = new Users(req.body);
+    user.save((err, userInfo) => { // mongoDB method.
+        if(err) return res.json({ success : false, err });
+        return res.status(200).json({ success : true });
+    });
 })
 
 app.listen(port, () => {
