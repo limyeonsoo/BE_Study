26c527443cde643e1dd011fb6f289ffe415b413c
limyeonsoo <yslim6168@naver.com>
2022-01-23 15:05:48 +0900

    [ADD] Initialize

---
+++ SpringMVC_API/src/test/java/paul/spring/mvc/practice/PracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package paul.spring.mvc.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
